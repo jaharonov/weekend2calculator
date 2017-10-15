289fdf4a77abfbe121195df605d84d70877e0082
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-15 12:16:33 -0500

    base mode complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('ready!');
 $('.calcType').on('click', getMath);
+$('#clear').on('click', clearFields);
 }
 
 function getMath(){
@@ -16,19 +17,36 @@ console.log(x);
 console.log(y);
 console.log(operation);
 
-
 //make a POST request
 $.ajax({
     method: 'POST', 
     url: '/doSomeMath',
     data: {value1: x, value2: y, value3: operation}
-})
-
-.done(function(response) {
+}).done(function(response) {
     console.log('successfully posted variables!');
     
-})
-.fail(function(msg, status) {
+}).fail(function(msg, status) {
     console.log('error response:', status);
-})
+});
+//GET request
+ $.ajax({
+    method: 'GET',
+   url: '/doSomeMath' 
+ }).done(function (response) {
+       console.log(response);
+       appendAnswer(response);
+  }) .fail(function (msg, status) {
+        console.log('error response:', status);
+   });
+
+}
+
+function appendAnswer(response) {
+
+    $('#answerZone').append('<p>' + response.answer + '</p>');
+}
+
+function clearFields() {
+    $('.values').val('');
+    $('#answerZone').text('');
 }
--- server/server.js
+++ server/server.js
@@ -4,45 +4,42 @@ var port = 5000;
 
 //serve static files
 
-app.use(express.static('server/public'));
+app.use(express.static('./server/public'));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/doSomeMath', function(req, res){
-   variables = (req.body);
-    res.sendStatus(201);
-    function doMath() {
-        var answer= 0;
-     if (variables.value3 === 'divi') {
-    answer = (parseInt(variables.value1) / parseInt(variables.value2));
-     }
-      else if (variables.value3 === 'add') {
-        answer = (parseInt(variables.value1) + parseInt(variables.value2));
-         }
-     else if (variables.value3 === 'sub') {
-         answer = (parseInt(variables.value1) - parseInt(variables.value2));
-         }
-     else  {
-          answer = (parseInt(variables.value1) * parseInt(variables.value2));
-         }
-         console.log({answer});
-         
-        };
-        
-     doMath();
-});
-
-
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('listening on port', port);
 });
 
-function doMath() {
-   if (value3 === 'divi') {
-       console.log('do division');
-   }
-   else {
-       console.log('do not do division');
-   };
-}
+
+app.post('/doSomeMath', function(req, res){
+   variables = (req.body);
+    res.sendStatus(201);
+    
+})
+
+     app.get('/doSomeMath', function (req, res) {
+       var answer =  doMath();
+         res.send({ answer });
+         console.log({ answer });
+     })
+     
+     function doMath() {
+         var answer = 0;
+         if (variables.value3 === 'divi') {
+             answer = (parseInt(variables.value1) / parseInt(variables.value2));
+         }
+         else if (variables.value3 === 'add') {
+             answer = (parseInt(variables.value1) + parseInt(variables.value2));
+         }
+         else if (variables.value3 === 'sub') {
+             answer = (parseInt(variables.value1) - parseInt(variables.value2));
+         }
+         else {
+             answer = (parseInt(variables.value1) * parseInt(variables.value2));
+         }
+         return answer;
+        }
+
